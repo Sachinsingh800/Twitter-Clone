54ee3be770beb1f4b4962d03e0b4e010ba0d04f6
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-01-18 13:33:58 +0530

    Date

--- src/Atom/SignUp/SignUp.js
+++ src/Atom/SignUp/SignUp.js
@@ -2,10 +2,10 @@ import React from 'react'
 import Image from './google.png'
 import style from './SignUp.module.css'
 
-function SignUp({SignUpTxt,Image}) {
+function SignUp({SignUpTxt,Image,handleClick}) {
   return (
     <div>
-        <button className={style.signbtn}><img className={style.img1} src={Image}></img>{SignUpTxt}</button>
+        <button onClick={handleClick} className={style.signbtn}><img className={style.img1} src={Image}></img>{SignUpTxt}</button>
     </div>
   )
 }
--- src/Components/Account/Account.js
+++ src/Components/Account/Account.js
@@ -5,10 +5,15 @@ import Inputfield from '../../Atom/Input/Input';
 import SignUp from '../../Atom/SignUp/SignUp';
 import { Link } from 'react-router-dom';
 import style from './Account.module.css'
+import MonthDropdown from './Month'
+import DayDropdown from './Day';
+import YearDropDown from './Year';
 
 
 function Account({label}) {
- 
+ function clicked(){
+  alert("Clicked")
+ }
   return (
     <div  className={style.main} >
     <form className={style.form}>
@@ -28,10 +33,19 @@ function Account({label}) {
             <h5 style={{fontWeight:550}}>Date of birth</h5>
             <h5 style={{marginTop:"-20px", fontWeight: "200"}}>This will not be shown publicly. Confirm your own age, even if this account is for a business, a pet, or something else. </h5>
         </div>
+
+        <div className={style.date}>
+       <span><MonthDropdown /></span> 
+        <span><DayDropdown/></span>
+        <span><YearDropDown /></span>
+        </div>
+        
        <span  className={style.login}>
        <SignUp
-         SignUpTxt={"Next"}/>
+         SignUpTxt={"Next"}
+         handleClick={clicked}/>
         </span> 
+        
     </form>
   
 </div>
