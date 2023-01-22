df2e1cf1a9570d6a72bd9c0f982df9ce25c096fc
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-01-22 11:30:19 +0530

    Like-message-repeate-bar

--- src/Components/HomePageMiddleSection/MiddleSectionCard/MiddleSectionCard.js
+++ src/Components/HomePageMiddleSection/MiddleSectionCard/MiddleSectionCard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image  from "../../../Assest/Image/Modi1.jpg"
 import style from './MiddleSectionCard.module.css'
 import Image2 from  '../../../Assest/Image/Modi.jpeg'
@@ -8,6 +8,24 @@ import {BsSuitHeart} from 'react-icons/bs';
 import {BiBarChart} from 'react-icons/bi';
 
 function MiddleSectionCard() {
+
+  const [count ,setCout] =useState(0)
+  const [count1 ,setCout1] =useState(0)
+  const [count2 ,setCout2] =useState(0)
+  const [count3 ,setCout3] =useState(0)
+
+  function counter(){
+    setCout(count +1)
+  }
+  function counter1(){
+    setCout1(count1 +1)
+  }
+  function counter2(){
+    setCout2(count2 +1)
+  }
+  function counter3(){
+    setCout3(count3 +1)
+  }
 
   return (
     <div className={style.main} >
@@ -23,10 +41,11 @@ function MiddleSectionCard() {
         <img  className={style.Img2} src={Image2} alt="Modi"/>
       </div>
       <div className={style.icon}>
-          <BiMessageRounded/>
-          <IoMdRepeat/>
-          <BsSuitHeart/>
-          <BiBarChart/>
+        <button className={style.button} onClick={counter}><BiMessageRounded/><span>{count}</span> </button>
+        <button className={style.button} onClick={counter1}><IoMdRepeat/><span>{count1}</span> </button>
+        <button className={style.button} onClick={counter2}><BsSuitHeart/><span>{count2}</span> </button>
+        <button className={style.button} onClick={counter3}><BiBarChart/><span>{count3}</span> </button>
+      
       </div>
     </div>
   )
