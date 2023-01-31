f530ce06a50b02ec645a33d2d6e9ead2c1c06535
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-01-31 16:07:58 +0530

    like button

--- src/Atom/LikeButton/LikeButton.js
+++ src/Atom/LikeButton/LikeButton.js
@@ -1,16 +1,17 @@
 
 import React, { useState } from 'react'
 import {BsSuitHeart} from 'react-icons/bs';
+import {FcLike} from 'react-icons/fc';
 import style from "./LikeButton.module.css"
 
 function LikeButton() {
-    const [count ,setCount] = useState(0)
+    const [isShow, setShow] = useState(false)
     function Counter(){
-        setCount(count +1)
+       setShow(!isShow)
     }
   return (
     <div>
-      <button className={style.button}  onClick={Counter}><span className={style.icon}><BsSuitHeart/></span>{count}</button>
+      <button className={style.button}  onClick={Counter}><span className={style.icon}>{isShow ? <span style={{fontSize:"20px"}}><FcLike/></span> : <BsSuitHeart/> }</span>{ isShow ? 1 : 0}</button>
     </div>
   )
 }
